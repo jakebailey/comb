32d5e2c20afbf86baf70d26a4fe561cef7ae7325
Jake Bailey <jacob.b.bailey@gmail.com>
2017-03-19 03:04:46 -0500

    EOF test

--- parser_test.go
+++ parser_test.go
@@ -1,31 +1,48 @@
 package comb
 
 import (
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRune(t *testing.T) {
-	p := Rune('a', 'b', 'c')
-	s := NewStringScanner("abcd")
-	var r Result
+	t.Run("unexpected rune", func(t *testing.T) {
+		p := Rune('a', 'b', 'c')
+		s := NewStringScanner("abcd")
+		var r Result
 
-	r, s = p.Parse(s)
-	assert.True(t, r.Matched())
-	assert.Equal(t, 'a', r.Runes[0])
+		r, s = p.Parse(s)
+		assert.True(t, r.Matched())
+		assert.Equal(t, 'a', r.Runes[0])
 
-	r, s = p.Parse(s)
-	assert.True(t, r.Matched())
-	assert.Equal(t, 'b', r.Runes[0])
+		r, s = p.Parse(s)
+		assert.True(t, r.Matched())
+		assert.Equal(t, 'b', r.Runes[0])
 
-	r, s = p.Parse(s)
-	assert.True(t, r.Matched())
-	assert.Equal(t, 'c', r.Runes[0])
+		r, s = p.Parse(s)
+		assert.True(t, r.Matched())
+		assert.Equal(t, 'c', r.Runes[0])
 
-	r, s = p.Parse(s)
-	assert.False(t, r.Matched())
-	assert.EqualError(t, r.Err, "unexpected rune 'd'")
+		r, s = p.Parse(s)
+		assert.False(t, r.Matched())
+		assert.EqualError(t, r.Err, "unexpected rune 'd'")
+	})
+
+	t.Run("EOF", func(t *testing.T) {
+		p := Rune('a', 'b', 'c')
+		s := NewStringScanner("a")
+		var r Result
+
+		r, s = p.Parse(s)
+		assert.True(t, r.Matched())
+		assert.Equal(t, 'a', r.Runes[0])
+
+		r, s = p.Parse(s)
+		assert.False(t, r.Matched())
+		assert.Equal(t, io.EOF, r.Err)
+	})
 }
 
 func BenchmarkRune(b *testing.B) {
