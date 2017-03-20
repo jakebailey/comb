ed1a2ebc2186bba7f8d9b6ea59ceeb242c3fc1ec
Jake Bailey <jacob.b.bailey@gmail.com>
2017-03-19 22:50:54 -0500

    add Many, OnePlus, and variants

---
+++ many.go
@@ -0,0 +1,102 @@
+package comb
+
+// Many looks for a series of 0+ matches of a parser,
+// then combines the results with a combiner. If combiner is nil,
+// SliceCombiner is used.
+//
+// If you only need the runes captured by Many, use TextMany instead.
+func Many(combiner ResultCombiner, parser Parser) Parser {
+	if combiner == nil {
+		combiner = SliceCombiner
+	}
+
+	return ParserFunc(func(s Scanner) (Result, Scanner) {
+		var results []Result
+		next := s
+
+		for {
+			r, maybeNext := parser.Parse(next)
+			if !r.Matched() {
+				break
+			}
+
+			next = maybeNext
+			results = append(results, r)
+		}
+
+		return combiner(results, s, next), next
+	})
+}
+
+// TextMany looks for a series of 0+ matches of a parser,
+// then returns the runes captured.
+func TextMany(parser Parser) Parser {
+	return ParserFunc(func(s Scanner) (Result, Scanner) {
+		next := s
+
+		for {
+			r, maybeNext := parser.Parse(next)
+			if !r.Matched() {
+				break
+			}
+			next = maybeNext
+		}
+
+		return Result{
+			Runes: s.Between(next),
+		}, next
+	})
+}
+
+// OnePlus looks for a series of 1+ matches of a parser.
+//
+// If you only need the runes captured by OnePlus, use TextOnePlus instead.
+func OnePlus(combiner ResultCombiner, parser Parser) Parser {
+	if combiner == nil {
+		combiner = SliceCombiner
+	}
+
+	return ParserFunc(func(s Scanner) (Result, Scanner) {
+		r, next := parser.Parse(s)
+		if !r.Matched() {
+			return r, next
+		}
+
+		results := []Result{r}
+
+		for {
+			r, maybeNext := parser.Parse(next)
+			if !r.Matched() {
+				break
+			}
+
+			next = maybeNext
+			results = append(results, r)
+		}
+
+		return combiner(results, s, next), next
+	})
+}
+
+// TextOnePlus looks for a series of 1+ matches of a parser,
+// then returns the runes captured.
+func TextOnePlus(parser Parser) Parser {
+	return ParserFunc(func(s Scanner) (Result, Scanner) {
+		r, next := parser.Parse(s)
+		if !r.Matched() {
+			return r, next
+		}
+
+		for {
+			r, maybeNext := parser.Parse(next)
+			if !r.Matched() {
+				break
+			}
+			next = maybeNext
+		}
+
+		return Result{
+			Runes: s.Between(next),
+		}, next
+	})
+}
---
+++ many_test.go
@@ -0,0 +1,147 @@
+package comb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMany(t *testing.T) {
+	p := Many(
+		nil,
+		Char('a'),
+	)
+
+	t.Run("match", func(t *testing.T) {
+		s := NewStringScanner("aaaab")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Interface: []Result{
+				{Runes: []rune("a")},
+				{Runes: []rune("a")},
+				{Runes: []rune("a")},
+				{Runes: []rune("a")},
+			},
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+
+	t.Run("empty", func(t *testing.T) {
+		s := NewStringScanner("bbbbbb")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Interface: []Result(nil),
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+}
+
+func TestTextMany(t *testing.T) {
+	p := TextMany(
+		Char('a'),
+	)
+
+	t.Run("match", func(t *testing.T) {
+		s := NewStringScanner("aaaab")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Runes: []rune("aaaa"),
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+
+	t.Run("empty", func(t *testing.T) {
+		s := NewStringScanner("bbbbbb")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Runes: []rune(""),
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+}
+
+func TestOnePlus(t *testing.T) {
+	p := OnePlus(
+		nil,
+		Char('a'),
+	)
+
+	t.Run("match", func(t *testing.T) {
+		s := NewStringScanner("aaaab")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Interface: []Result{
+				{Runes: []rune("a")},
+				{Runes: []rune("a")},
+				{Runes: []rune("a")},
+				{Runes: []rune("a")},
+			},
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+
+	t.Run("no match", func(t *testing.T) {
+		s := NewStringScanner("bbbbbb")
+
+		r, next := p.Parse(s)
+
+		assert.False(t, r.Matched())
+		assert.EqualError(t, r.Err, "unexpected character 'b'")
+		assert.False(t, next.EOF())
+	})
+}
+
+func TestTextOnePlus(t *testing.T) {
+	p := TextOnePlus(
+		Char('a'),
+	)
+
+	t.Run("match", func(t *testing.T) {
+		s := NewStringScanner("aaaab")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Runes: []rune("aaaa"),
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+
+	t.Run("no match", func(t *testing.T) {
+		s := NewStringScanner("bbbbbb")
+
+		r, next := p.Parse(s)
+
+		assert.False(t, r.Matched())
+		assert.EqualError(t, r.Err, "unexpected character 'b'")
+		assert.False(t, next.EOF())
+	})
+}
