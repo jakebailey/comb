e44483219974fd3744b5c91104f91a4e2ad778dd
Jake Bailey <jacob.b.bailey@gmail.com>
2017-03-20 18:30:46 -0500

    add Regexp parser, example gitignore

---
+++ regexp.go
@@ -0,0 +1,70 @@
+package comb
+
+import (
+	"regexp"
+	"unicode/utf8"
+)
+
+// Regexp compiles a Go regexp into a parser. If the pattern does
+// not begin with ^, one will be added, as the parser must begin
+// with the next rune.
+func Regexp(pattern string) Parser {
+	realPattern := pattern
+	if realPattern == "" || realPattern[0] != '^' {
+		realPattern = "^" + realPattern
+	}
+
+	re := regexp.MustCompile(realPattern)
+
+	return ParserFunc(func(s Scanner) (Result, Scanner) {
+		sr := &scannerReader{s}
+
+		match := re.FindReaderIndex(sr)
+		if match == nil {
+			return Failedf("regexp %q did not match", pattern), s
+		}
+
+		var r rune
+		next := s
+		var err error
+
+		count := match[1]
+
+		if count == 0 {
+			return Result{}, s
+		}
+
+		for {
+			r, next, err = next.Next()
+			if err != nil {
+				return Failed(err), next
+			}
+
+			count -= utf8.RuneLen(r)
+
+			if count == 0 {
+				break
+			}
+			if count < 0 {
+				panic("bug: got more bytes than regexp match specified")
+			}
+		}
+
+		return Result{
+			Runes: s.Between(next),
+		}, next
+	})
+}
+
+type scannerReader struct {
+	next Scanner
+}
+
+func (s *scannerReader) ReadRune() (rune, int, error) {
+	r, next, err := s.next.Next()
+	if err != nil {
+		return 0, 0, err
+	}
+	s.next = next
+	return r, utf8.RuneLen(r), nil
+}
---
+++ regexp_test.go
@@ -0,0 +1,71 @@
+package comb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegexp(t *testing.T) {
+	p := Regexp(`[^\.]+`)
+
+	t.Run("match", func(t *testing.T) {
+		s := NewStringScanner("Hello, 世界.")
+
+		r, next := p.Parse(s)
+
+		expected := Result{
+			Runes: []rune("Hello, 世界"),
+		}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+
+	t.Run("no match", func(t *testing.T) {
+		s := NewStringScanner("... Hello, 世界.")
+
+		r, _ := p.Parse(s)
+
+		assert.False(t, r.Matched())
+		assert.EqualError(t, r.Err, `regexp "[^\\.]+" did not match`)
+	})
+
+	t.Run("empty", func(t *testing.T) {
+		p := Regexp("")
+		s := NewStringScanner("Hello, 世界.")
+
+		r, next := p.Parse(s)
+
+		expected := Result{}
+
+		assert.True(t, r.Matched())
+		assert.Equal(t, expected, r)
+		assert.False(t, next.EOF())
+	})
+}
+
+func BenchmarkRegexp(b *testing.B) {
+	b.ReportAllocs()
+
+	p := Regexp(`[^\.]+`)
+	s := NewStringScanner("Hello, 世界.")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Parse(s)
+	}
+}
+
+func BenchmarkRegexpManyTokens(b *testing.B) {
+	b.ReportAllocs()
+
+	p := Regexp(`(foobar|foolol|hello)`)
+	s := NewStringScanner("foobarbaz")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Parse(s)
+	}
+}
