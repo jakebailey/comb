d373ffeec3de327ab8a26ce433fe046f73e4f616
Jake Bailey <jacob.b.bailey@gmail.com>
2017-03-20 22:57:54 -0500

    combext docs

---
+++ combext/doc.go
@@ -0,0 +1,7 @@
+/*
+Package combext holds various comb helpers, including things like
+digit, alpha, whitespace, and integer parsers.
+*/
+package combext
+
+//go:generate sh -c "godoc2md github.com/jakebailey/comb/combext > README.md"
